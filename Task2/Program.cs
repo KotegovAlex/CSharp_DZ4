9ae1c94ff22ae3121bba0b55b31487a60d0f113c
Alexander Kotegov <s.kotegov2010@yandex.ru>
2022-07-05 10:30:35 +0500

    added Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,27 @@
+﻿/* Задача 27: Напишите программу, которая принимает на вход 
+число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12 */
+
+// ввод числа
+string Promt(string msg)
+{
+    System.Console.WriteLine(msg);
+    string number = Console.ReadLine();
+    return number;
+}
+// сумма цифр цисла
+int SumDigits(string number)
+{
+    int summ = 0;
+    for (int i = 0; i < number.Length; i++)
+    {
+        summ = summ + int.Parse(number[i].ToString());
+    }
+    return summ;
+}
+
+string number = Promt("Введите число N => ");
+int result = SumDigits(number);
+System.Console.WriteLine($"Сумма цифр цисла {number} равна => {result}");
